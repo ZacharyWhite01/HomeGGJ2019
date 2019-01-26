2badb67154f9b22eeff92edb2346e3d65e4a3b39
Zachary <WhiteLZachary@gmail.com>
2019-01-25 22:56:10 -0700

    Terrain
    Created Terrain
    Created Character Movement
    Added Snow Projection
    Working on Character Animation

---
+++ HomeGGJ2019/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CharacterMovement : MonoBehaviour
+{
+    Camera cam;
+    public LayerMask movementMask;
+    NavMeshAgent agent;
+    Animator anim;
+    Vector3 destination;
+    bool isWalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                destination = hit.point;
+                agent.SetDestination(destination);
+                anim.SetTrigger("Walking");
+
+            }
+        }
+        if (destination != null && transform.position == destination)
+        {
+            anim.SetTrigger("Standing");
+        }
+    }
+}
---
+++ HomeGGJ2019/Assets/Scripts/SeasonTerrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonTerrain : MonoBehaviour
+{
+    TerrainData terrainData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrainData = GetComponent<TerrainData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
