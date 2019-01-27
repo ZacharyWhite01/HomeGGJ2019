6f99e1365b2c6d71de8d39628961b3be0befc56a
Zachary <WhiteLZachary@gmail.com>
2019-01-26 20:05:57 -0700

    FirstPersonMovement
    Added FirstPersonMovement Support

--- HomeGGJ2019/Assets/Scripts/CameraController.cs
+++ HomeGGJ2019/Assets/Scripts/CameraController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Camera firstPersonCamera;
+    public Camera topDownCamera;
+
     public Transform target;
     public Vector3 offset;
-    bool canSee = false;
 
     public float zoomSpeed = 4f;
     public float minZoom = 5f;
@@ -18,32 +20,73 @@ public class CameraController : MonoBehaviour
     private float currentZoom = 10f;
     private float currentYaw = 0f;
 
+    public bool camSwitch = false;
+
+    public float minX = -60.0f;
+    public float maxX = 60.0f;
+    public float minY = -360.0f;
+    public float maxY = 360.0f;
+
+    public float sensitivityX = 10f;
+    public float sensitivityY = 10f;
+
+    float rotationX = 0f;
+    float rotationY = 0f;
+
+    public CharacterMovement charMovement;
+
     // Start is called before the first frame update
+    private void Start()
+    {
+        topDownCamera.gameObject.SetActive(false);
+        firstPersonCamera.gameObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
-        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+        if (topDownCamera.gameObject.activeSelf == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+            currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
 
-        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
-        if (target.position.z <= 187f && !canSee)
-        {
-            Camera.main.cullingMask = Camera.main.cullingMask ^ (1 << 11);
-            canSee = true;
+            currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+            CameraChanged();
         }
-        else
+
+        if (firstPersonCamera.gameObject.activeSelf == true)
         {
-            Camera.main.cullingMask = Camera.main.cullingMask ^ (1 << 11);
-            canSee = false;
+            rotationX += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY += Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationX = Mathf.Clamp(rotationX, minX, maxX);
+
+            transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+            target.localEulerAngles = new Vector3(0, rotationY, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            camSwitch = !camSwitch;
+            firstPersonCamera.gameObject.SetActive(!camSwitch);
+            topDownCamera.gameObject.SetActive(camSwitch);
+            CameraChanged();
+            charMovement.GetComponent<CharacterMovement>().detectCam();
         }
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+
+    void CameraChanged()
     {
-        transform.position = target.position - (offset * currentZoom);
+        if (topDownCamera.gameObject.activeSelf == true)
+        {
+            topDownCamera.gameObject.transform.position = target.position - (offset * currentZoom);
 
-        transform.LookAt(target.position + (Vector3.up * pitch));
+            topDownCamera.gameObject.transform.LookAt(target.position + (Vector3.up * pitch));
 
-        transform.RotateAround(target.position, Vector3.up, currentYaw);
+            topDownCamera.gameObject.transform.RotateAround(target.position, Vector3.up, currentYaw);
+        }
     }
 }
--- HomeGGJ2019/Assets/Scripts/CharacterMovement.cs
+++ HomeGGJ2019/Assets/Scripts/CharacterMovement.cs
@@ -11,12 +11,18 @@ public class CharacterMovement : MonoBehaviour
     NavMeshAgent agent;
     Animator anim;
     Vector3 destination;
+
     bool isWalking = false;
+    bool firstPerson = false;
+    public int speed = 100;
+
+    Vector3 moveDirection;
+
+    public CameraController mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main;
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
     }
@@ -24,25 +30,85 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (cam != null)
         {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            if (firstPerson == false)
             {
-                destination = hit.point;
-                agent.SetDestination(destination);
-            }
-        }
-        if (agent.remainingDistance <= agent.stoppingDistance)
-        {
-            isWalking = false;
+                if (agent.enabled == false)
+                {
+                    agent.enabled = true;
+                    agent.isStopped = false;
+                    agent.SetDestination(transform.position);
+                    GetComponent<Rigidbody>().isKinematic = true;
+                    agent.isStopped = false;
+                }
 
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit, 100, movementMask))
+                    {
+                        destination = hit.point;
+                        agent.SetDestination(destination);
+                    }
+                }
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    isWalking = false;
+                }
+                else
+                {
+                    isWalking = true;
+                }
+            }
+
+            if (firstPerson)
+            {
+                if (agent.enabled)
+                {
+                    agent.isStopped = true;
+                    agent.enabled = false;
+                }
+                Vector3 pos = transform.position;
+
+                float horizontalMovement = Input.GetAxis("Horizontal");
+                float verticleMovement = Input.GetAxis("Vertical");
+
+                moveDirection = (horizontalMovement * transform.right + verticleMovement * transform.forward).normalized;
+
+                GetComponent<Rigidbody>().isKinematic = false;
+
+                Move();
+            }
+            anim.SetBool("Walking", isWalking);
         }
         else
         {
-            isWalking = true;
+            detectCam();
         }
-        anim.SetBool("Walking", isWalking);
+    }
+
+    public void detectCam()
+    {
+
+        if (mainCamera.firstPersonCamera.gameObject.activeSelf == true)
+        {
+            firstPerson = true;
+            cam = mainCamera.firstPersonCamera;
+        }
+        else if (mainCamera.topDownCamera.gameObject.activeSelf == true)
+        {
+            firstPerson = false;
+            cam = mainCamera.topDownCamera;
+        }
+    }
+
+    void Move()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 yVelocity = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = moveDirection * speed * Time.deltaTime;
+        rb.velocity += yVelocity;
     }
 }
