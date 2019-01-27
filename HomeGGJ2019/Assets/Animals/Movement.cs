c2088fb14b3acc238b7793ce7e1cb0278788240a
Zachary <WhiteLZachary@gmail.com>
2019-01-26 21:14:16 -0700

    Animals
    Animals
    Prefabs/Animations
    Added

---
+++ HomeGGJ2019/Assets/Animals/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+
+    private NavMeshAgent block;
+    private GameObject dan;
+   
+    void Start()
+    {
+        block = GetComponent<NavMeshAgent>();
+        dan = GameObject.Find("Dan");
+
+    }
+
+
+    void Update()
+    {
+        block.SetDestination(dan.transform.position);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (dan.gameObject.name == "Dan")
+        {
+            SceneManager.LoadScene("endGame");
+        }
+
+    }
+}
+// Targets name is DAN
