fe5c06813621d8ef00fdb889d97956d6a566f57c
Zachary <WhiteLZachary@gmail.com>
2019-01-26 21:29:36 -0700

    Implementing Animals
    Adding Animals with the follow

---
+++ HomeGGJ2019/Assets/Austin/movement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class movement : MonoBehaviour
+{
+    private Terrain terrain;
+    private NavMeshAgent agent;
+    float minX, minZ, maxX, maxZ;
+    private GameObject mainPlayer;
+    public NavMeshAgent[] followChain = new NavMeshAgent[7];
+    int x = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GameObject landscape = GameObject.Find("Terrain");
+        terrain = landscape.GetComponent<Terrain>();
+        Debug.Log(terrain.terrainData.bounds.size);
+        mainPlayer = GameObject.Find("Player");
+
+    }
+    float i = 1.0f;
+    int i2 = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        double distance = Vector3.Distance(transform.position, mainPlayer.transform.position);
+        Debug.Log(distance);
+        if (distance <= 10)
+        {
+            for (x = 0; x < followChain.Length; x++)
+            {
+                if (x == 0)
+                {
+                    agent.SetDestination(mainPlayer.transform.position + new Vector3(-1, 0, 0));
+                }
+                else
+                {
+                    agent.SetDestination(followChain[x - 1].transform.position + new Vector3(-1, 0, 0));
+                }
+            }
+        }
+        else
+        {
+            i -= Time.deltaTime;
+            if (i <= 0.0f)
+            {
+                i = 1.0f;
+                Debug.Log("A");
+                if (i2 == 0)
+                {
+                    i2 = 5;
+                    Vector3 three = new Vector3(Random.Range(terrain.terrainData.bounds.min.x, terrain.terrainData.bounds.max.x),
+    0, Random.Range(terrain.terrainData.bounds.min.z, terrain.terrainData.bounds.max.z));
+
+
+                    agent.SetDestination(three);
+                }
+
+                i2--;
+
+            }
+        }
+    }
+}
